43102a7ea14a1a408ec227d4552ba27e88d048b6
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-20 12:59:44 +0530

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -1,9 +1,16 @@
 class TreeNode:
     def __init__(self,data):
         self.data=data
-        salf.children=[]
+        self.children=[]
         self.parent=None
-        
+
     def add_child(self,child):
         child.parent=self
         self.children.append(child)
+
+
+def build_product():
+    root=Treenode("Electronice")
+    laptop = TreeNode("Laptop")
+    root.add_child(laptop)
+
