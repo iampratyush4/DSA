07405927c8cb24fdcc90ac43b8e4f5d5ec92e30e
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-20 20:01:43 +0530

    Update deleteBSTnode.py

--- deleteBSTnode.py
+++ deleteBSTnode.py
@@ -1,3 +1,20 @@
+
+"""Conditions for deletion
+    condtions 1 - deleting node is leaf node
+        then there is no issue we can directly delete it.
+    condition 2- non leaf with single child.
+        we can join its child directly to its grandparent node
+
+    condition 3 - if deleting node has 2 childs
+        we can either take the maximum value in this nodes left subtree and place it on its position or we can take the minimum
+        value from right subtree and place it in place of deleting node.
+"""
+
+
+
+
+
+
 class BST:
     def __init__(self,data):
         self.data=data
@@ -38,7 +55,7 @@ class BST:
         if self.left is None:
             return self.right
         if self.right is None:
-            return self.left
+            return self.left    
 
 def build_tree(elements):
     root= BST(elements[0])
