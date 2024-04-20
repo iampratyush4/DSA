5d96d6fbefff2638252c5dfc4a331b35c17ab558
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-20 18:56:52 +0530

    Update deleteBSTnode.py

--- deleteBSTnode.py
+++ deleteBSTnode.py
@@ -3,5 +3,31 @@ class BST:
         self.data=data
         self.left=None
         self.right=None
+
+    def add_child(self,data):
+        if self.data ==data:
+            return
+        if data<self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left=BST(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right= BST(data)
+
+def build_tree(elements):
+    root= BST(elements[0])
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+    return root
+
+
+
+
+
         
+
         
