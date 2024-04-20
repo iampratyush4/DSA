ee4c22d0c492bde311bb20fe3343f6a7f2d71e52
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-21 00:00:41 +0530

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -4,3 +4,26 @@ types of graphs -
     2-undirected
     3-weighted graphs(ex- distance ,weights etc)
 """
+
+class Graph:
+    def __init__(self,edges):
+        self.edges=edges
+        self.graph_dict={}
+        for start,end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start]=[end]
+        print("Graph dict is" , self.graph_dict)
+
+if __name__ =='__main__':
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
+    ]
+
+route_graph=Graph(routes)
