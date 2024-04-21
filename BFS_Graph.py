da6b3598cc234fb9871fe2adcf03a6b3d4e250cb
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-21 13:38:55 +0530

    Update BFS_Graph.py

---
+++ BFS_Graph.py
@@ -0,0 +1,12 @@
+"""
+it uses queue
+it is a traversal technique to list down all the nodes in a graph.
+Method 1-
+    1- take any node from a graph
+    2-it a node is connected twice then remove any one conenction
+    3- now a spanning tree woul have been formed.
+    4- now we have to just find the level order traversal of this tree
+    5- and this traversal result would be the BFS answer
+
+
+"""
