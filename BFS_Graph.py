bb0cb81fc3d736a8fb90f965251115b2879a93e4
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-21 17:54:19 +0530

    Update BFS_Graph.py

--- BFS_Graph.py
+++ BFS_Graph.py
@@ -1,9 +1,10 @@
 """
+
 it uses queue
 it is a traversal technique to list down all the nodes in a graph.
 Method 1-
     1- take any node from a graph
-    2-it a node is connected twice then remove any one conenction
+    2- it a node is connected twice then remove any one conenction
     3- now a spanning tree woul have been formed.
     4- now we have to just find the level order traversal of this tree
     5- and this traversal result would be the BFS answer
@@ -16,20 +17,24 @@ Method 2 -
 
 import collections
 
-
 def bfs(graph,startnode):
+
     visited=set()
     queue=collections.deque([startnode])
+
     # when we deque a element from a queue we will insert it into the visited set
+
     while queue:
-        popedNode=queue.popleft()
+        popedNode = queue.popleft()
         visited.add(popedNode)
         for i in graph[popedNode]:
-            
-
-    return
+            if i not in visited:
+                queue.append(i)
+                
+    return visited
 
 if __name__=="__main__":
+
     graph={
         0:[1,2,3],
         1:[0,2],
@@ -37,5 +42,6 @@ if __name__=="__main__":
         3:[0],
         4:[2]
     }
+
     nodetostart=0
-    bfs(graph,nodetostart)
+    print(bfs(graph,nodetostart))
