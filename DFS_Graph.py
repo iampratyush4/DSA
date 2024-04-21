028fbf98da682379970b16e1e2f6a2c809b92ecf
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-21 18:39:23 +0530

    Create DFS_Graph.py

---
+++ DFS_Graph.py
@@ -0,0 +1,23 @@
+
+def dfs(visited,graph,startnode):
+    if startnode not in visited:
+        print(startnode)
+        visited.add(startnode)
+        for neighbor in graph[startnode]:
+            dfs(visited,graph,neighbor)
+
+
+
+if __name__=="__main__":
+
+    graph={
+        'A':['B','C','D'],
+        'B':['E'],
+        'C':['D','E'],
+        'D':[],
+        'E':[]
+    }
+    visited=set()
+
+    nodetostart='A'
+    print(dfs(visited,graph,nodetostart))
