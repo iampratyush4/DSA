f3b073fd81a183844badaf9be197780e9feedb99
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-21 08:38:53 +0530

    adjacent

--- graphs.py
+++ graphs.py
@@ -16,6 +16,25 @@ class Graph:
                 self.graph_dict[start]=[end]
         print("Graph dict is" , self.graph_dict)
 
+    def get_paths(self,start,end,path=[]):
+        path =path + [start]
+        # if start and end points are same then return itself
+        if start==end:
+            return [path]
+        # if origin itself is not present in graph then return empty list
+        if start not in self.graph_dict:
+            return []
+        paths=[]
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths=self.get_paths(node,end,path)
+                for p in new_paths:
+                    paths.append(p)
+                    
+        return paths
+
+
+
 if __name__ =='__main__':
     routes = [
         ("Mumbai", "Paris"),
@@ -25,5 +44,9 @@ if __name__ =='__main__':
         ("Dubai", "New York"),
         ("New York", "Toronto"),
     ]
+    start="Mumbai"
+    end="New York"
+
 
 route_graph=Graph(routes)
+print(f" Paths between {start} and {end} :" , route_graph.get_paths(start,end))
