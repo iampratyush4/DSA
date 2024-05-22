72ed8b643fd7462c1b5a351c2122bb4f00942a56
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-05-22 20:01:57 +0530

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -18,7 +18,7 @@ class Graph:
 
     def get_paths(self,start,end,path=[]):
         path =path + [start]
-        # if start and end points are same then return itselfss
+        # if start and end points are same then return itselfsss
         if start==end:
             return [path]
         # if origin itself is not present in graph then return empty listss
